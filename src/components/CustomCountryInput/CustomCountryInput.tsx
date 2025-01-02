841b28b4fa19aef79a28620bd124e7d8438a9616
Ubaid Ullah Manzoor <ubaidmanzoor987@gmail.com>
2025-01-02 11:34:10 +0500

    updated custom country input

--- src/components/CustomCountryInput/CustomCountryInput.tsx
+++ src/components/CustomCountryInput/CustomCountryInput.tsx
@@ -1,43 +1,110 @@
 import { PhoneInput } from "react-international-phone";
 import "react-international-phone/style.css";
+import { PhoneNumberUtil } from "google-libphonenumber";
+import { useState } from "react";
+
 import { COLORS } from "@muc/constant";
+import { InputAdornment, Typography } from "@mui/material";
+import { Info } from "@mui/icons-material";
 
 type CountryPhoneInputProps = {
   value: string;
   onChange: (value: string) => void;
+  onBlur?: () => void;
+  readOnly?: boolean;
+  error?: string;
 };
 
-const CountryPhoneInput = ({ value, onChange }: CountryPhoneInputProps) => (
-  <PhoneInput
-    value={value}
-    onChange={onChange}
-    defaultCountry="us"
-    placeholder="Enter Phone Number"
-    inputStyle={{
-      width: "100%",
-      border: "none",
-      borderRadius: "8px",
-      fontSize: "14px",
-      outline: "none",
-      color: COLORS.primary.darkCyanBlue,
-    }}
-    style={{
-      border: `1px solid ${COLORS.primary.main}`,
-      borderRadius: "8px",
-      backgroundColor: "white",
-      display: "flex",
-      alignItems: "center",
-      height: 52,
-      padding: "0 12px",
-    }}
-    countrySelectorStyleProps={{
-      buttonStyle: {
-        border: "none",
-        borderRadius: "8px",
-        zIndex: 1000,
-      },
-    }}
-  />
-);
+const phoneUtil = PhoneNumberUtil.getInstance();
+
+const isPhoneValid = (phone: string) => {
+  try {
+    return phoneUtil.isValidNumber(phoneUtil.parseAndKeepRawInput(phone));
+  } catch (error) {
+    return false;
+  }
+};
+
+const CountryPhoneInput = ({
+  value,
+  onChange,
+  onBlur,
+  error,
+  readOnly = false,
+}: CountryPhoneInputProps) => {
+  const [isValid, setIsValid] = useState(true);
+
+  const handleChange = (phone: string) => {
+    setIsValid(isPhoneValid(phone));
+    onChange(phone);
+  };
+
+  const [isTouch, setIsTouch] = useState(false);
+
+  return (
+    <>
+      <PhoneInput
+        value={value}
+        onChange={handleChange}
+        onBlur={onBlur}
+        onFocus={() => setIsTouch(true)}
+        defaultCountry="us"
+        placeholder="Enter Phone Number"
+        inputProps={{
+          readOnly: readOnly,
+        }}
+        inputStyle={{
+          width: "100%",
+          border: "none",
+          borderRadius: "8px",
+          fontSize: "14px",
+          outline: "none",
+          color: COLORS.primary.main,
+        }}
+        style={{
+          border: `1px solid ${
+            error ? COLORS.red.main : COLORS.primary.main
+          }`,
+          borderRadius: "8px",
+          backgroundColor: "white",
+          display: "flex",
+          alignItems: "center",
+          height: 52,
+          padding: "0 12px",
+        }}
+        countrySelectorStyleProps={{
+          buttonStyle: {
+            border: "none",
+            borderRadius: "8px",
+            zIndex: 1000,
+          },
+        }}
+      />
+      {(error && error.length > 0) || (!isValid && isTouch) ? (
+        <Typography
+          variant="caption"
+          component="span"
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            color: COLORS.red.main,
+            textTransform: "capitalize",
+          }}
+        >
+          <InputAdornment position="start">
+            <Info
+              sx={{
+                color: COLORS.red.main,
+                rotate: "180deg",
+                width: 16,
+              }}
+            />
+          </InputAdornment>
+          {!isValid && isTouch ? "Invalid phone number" : error}
+        </Typography>
+      ) : null}
+    </>
+  );
+};
 
 export default CountryPhoneInput;
--- src/pages/ManageAuth/TwoFactorAuthPhone/TwoFactorAuthPhone.tsx
+++ src/pages/ManageAuth/TwoFactorAuthPhone/TwoFactorAuthPhone.tsx
@@ -1,60 +1,70 @@
-import { CustomTextField, CommonButton } from '@muc/components';
-import React from 'react';
-import { useForm, FormProvider, SubmitHandler } from 'react-hook-form';
-import { COLORS } from '@muc/constant';
-import { ROUTES } from '@muc/routes';
-import { ButtonWrapper, Heading, StyledContainer } from '../login/Login.styles';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import {
+  useForm,
+  FormProvider,
+  SubmitHandler,
+  Controller,
+} from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { CustomCountryInput, CommonButton } from "@muc/components";
+import { COLORS } from "@muc/constant";
+import { ROUTES } from "@muc/routes";
+import { ButtonWrapper, Heading, StyledContainer } from "../login/Login.styles";
+import { useNavigate } from "react-router-dom";
 
-interface TwoFactorAuthPhoneProps { }
+interface TwoFactorAuthPhoneProps {}
 
 interface FormData {
-    phone: string;
+  phone: string;
 }
 
-const TwoFactorAuthPhone: React.FC<TwoFactorAuthPhoneProps> = (): React.ReactElement => {
-    const methods = useForm<FormData>();
-    const navigate = useNavigate();
+const schema = yup.object().shape({
+  phone: yup.string().required("Phone number is required"),
+});
 
-    const onSubmit: SubmitHandler<FormData> = (data) => {
-        if (data.phone === '+1234567891') {
-            navigate(ROUTES.AUTH.TWO_FACTOR_AUTH_PIN);
-        } else {
-            alert('Invalid phone number');
-        }
-    };
+const TwoFactorAuthPhone: React.FC<
+  TwoFactorAuthPhoneProps
+> = (): React.ReactElement => {
+  const methods = useForm<FormData>({
+    resolver: yupResolver(schema),
+  });
+  const navigate = useNavigate();
 
-    return (
-        <StyledContainer>
-            <Heading variant="h2" gutterBottom>
-                2FA
-            </Heading>
-            <Heading variant="body1" gutterBottom>
-                To set up 2FA for your account, please enter your phone number
-            </Heading>
-            <FormProvider {...methods}>
-                <form noValidate onSubmit={methods.handleSubmit(onSubmit)}>
-                    <CustomTextField
-                        name="phone-number"
-                        label="Phone number"
-                        type="text"
-                        placeHolder="+1 000 000 0000"
-                        width="100%"
-                        rules={{ required: 'Required' }}
-                    />
-                    <ButtonWrapper>
-                        <CommonButton
-                            type="submit"
-                            variant="contained"
-                            title="Submit"
-                            width="100%"
-                            bgColor={COLORS.blue.navyBlue}
-                        />
-                    </ButtonWrapper>
-                </form>
-            </FormProvider>
-        </StyledContainer>
-    );
-}
+  const onSubmit: SubmitHandler<FormData> = (_data) => {
+    navigate(ROUTES.AUTH.TWO_FACTOR_AUTH_PIN);
+  };
+
+  return (
+    <StyledContainer>
+      <Heading variant="h2" gutterBottom>
+        2FA
+      </Heading>
+      <Heading variant="body1" gutterBottom>
+        To set up 2FA for your account, please enter your phone number
+      </Heading>
+      <FormProvider {...methods}>
+        <form onSubmit={methods.handleSubmit(onSubmit)}>
+          <Controller
+            name="phone"
+            control={methods.control}
+            render={({ field, formState: { errors } }) => (
+              <CustomCountryInput {...field} error={errors.phone?.message} />
+            )}
+          />
+          <ButtonWrapper>
+            <CommonButton
+              type="submit"
+              variant="contained"
+              title="Submit"
+              width="100%"
+              bgColor={COLORS.blue.navyBlue}
+            />
+          </ButtonWrapper>
+        </form>
+      </FormProvider>
+    </StyledContainer>
+  );
+};
 
 export default TwoFactorAuthPhone;
